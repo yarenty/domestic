7cccd2e9695bb3ff8b10cc05324a4ce99b01686a
Jaroslaw Nowosad <yarenty@gmail.com>
2025-02-06 13:39:23 +0000

    update Poc installation

--- crates/web/migration/src/lib.rs
+++ crates/web/migration/src/lib.rs
@@ -4,6 +4,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_users;
 
+mod m20250203_174659_domestics;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -12,6 +13,7 @@ impl MigratorTrait for Migrator {
         vec![
             // inject-below (do not remove this comment)
             Box::new(m20220101_000001_users::Migration),
+            Box::new(m20250203_174659_domestics::Migration),
             // inject-above (do not remove this comment)
         ]
     }
